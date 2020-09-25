7f788546a3d65bccd2d4a8f5cb2a9d54a2be61a6
mstucki6 <marcusstucki@gmail.com>
2020-09-25 16:49:58 -0600

    Update script.js

--- hw3/script.js
+++ hw3/script.js
@@ -5,7 +5,20 @@
  */
 function staircase() {
   // ****** TODO: PART II ******
-
+  let g = document.getElementById("aBarChart") //select the chart id
+  let getter = []
+  for (let rect of g.children){
+    let width = rect.getAttribute('width')
+    getter.push(width)
+    
+    //console.log(rev)
+      }
+  let rev = getter.sort((a, b)=>a-b) 
+  counter = 0
+  for (let rect of g.children){
+    rect.setAttribute("width", rev[counter]) //gives value at the index
+    counter ++
+  }
 }
 
 /**
@@ -51,12 +64,14 @@ function update(data) {
     .scaleLinear()
     .domain([0, data_length])
     .range([0, 260]);
-  
+
   // Draw axis for Bar Charts, Line Charts and Area Charts (You don't need to change this part.)
   d3.select("#aBarChart-axis").attr("transform", "translate(0,210)").call(d3.axisBottom(d3.scaleLinear().domain([0, d3.max(data, d => d.cases)]).range([barChart_width, barChart_width-maxBar_width])).ticks(5));
   d3.select("#aAreaChart-axis").attr("transform", "translate(0,245)").call(d3.axisBottom(d3.scaleLinear().domain([0, d3.max(data, d => d.cases)]).range([areaChart_width, areaChart_width-maxBar_width])).ticks(5));
+  
   d3.select("#bBarChart-axis").attr("transform", "translate(5,210)").call(d3.axisBottom(bScale).ticks(5));
   d3.select("#bAreaChart-axis").attr("transform", "translate(5,245)").call(d3.axisBottom(bScale).ticks(5));
+ 
   let aAxis_line = d3.axisLeft(aScale).ticks(5);
   d3.select("#aLineChart-axis").attr("transform", "translate(50,15)").call(aAxis_line);
   d3.select("#aLineChart-axis").append("text").text("New Cases").attr("transform", "translate(50, -3)")
@@ -67,16 +82,76 @@ function update(data) {
   // ****** TODO: PART III (you will also edit in PART V) ******
 
   // TODO: Select and update the 'a' bar chart bars
+  d3.select("#aBarChart").selectAll("rect")
+  .data(data)
+  .join("rect")
+ .attr("width", d => aScale(d.cases)) //Pass in cases and send through scaler
+  .attr("transform", (d, i) => `translate(0, ${i*14}) scale(-1,1)`) //Sets the transform attribute
+  .attr("height", 12)
+  
+  .on('mouseover', function (d, i){
+    d3.select(this)
+      .attr('class', 'hovered')
+  })
+  .on('mouseout', function (d, i){
+    d3.select(this)
+      .attr('class', '')
+  })
+
+  //Transition
+  // let svg = d3.select("svg")
+  // //data binding
+  // let aChart = svg.selectAll(".bar-chart bar-chart-x")
+  //   .data(data)
+  // //Transition new elements in with transparent grey bars of width 0
+  // let newA = aChart.enter().append("rect")
+  //   .attr("x", 0)
+  //   .attr("y", (d, i) => i *14)
+  //   .attr("width", 0)
+  //   .attr("height", 20)
+  //   .style("opacity", 0)
+  //   .classed("bar-chart bar-chart-x", true);
+  
 
   // TODO: Select and update the 'b' bar chart bars
+  d3.select("#bBarChart").selectAll("rect")
+  .data(data)
+  .join("rect")
+  .attr("width", d => bScale(d.deaths)) //Pass in cases and send through scaler
+  .attr("height", 12)
+  .attr("transform", (d, i) => `translate(0, ${i*14})`) //Sets the transform attribute
+  
+  .on('mouseover', function (d, i){
+    d3.select(this)
+      .attr('class', 'hovered')
+  })
+  .on('mouseout', function (d, i){
+    d3.select(this)
+      .attr('class', '')
+  });
+  
 
   // TODO: Select and update the 'a' line chart path using this line generator
-  let aLineGenerator = d3
-    .line()
+  let aLineGenerator = d3.
+    line()
     .x((d, i) => iScale_line(i))
     .y(d => aScale(d.cases));
 
+  d3.select("#aLineChart")
+    .join("path")
+    .attr('d', aLineGenerator(data))
+
+  
+
   // TODO: Select and update the 'b' line chart path (create your own generator)
+  let bLineGenerator = d3.
+  line()
+  .x((d, i) => iScale_line(i))
+  .y(d => bScale(d.deaths));
+
+d3.select("#bLineChart")
+  .join("path")
+  .attr('d', bLineGenerator(data))
 
   // TODO: Select and update the 'a' area chart path using this area generator
   let aAreaGenerator = d3
@@ -85,10 +160,51 @@ function update(data) {
     .y0(0)
     .y1(d => aScale(d.cases));
 
+  d3.select("#aAreaChart")
+  .join("path")
+  .attr('d', aAreaGenerator(data))
+
+
   // TODO: Select and update the 'b' area chart path (create your own generator)
 
+  let bAreaGenerator = d3
+  .area()
+  .x((d, i) => iScale_area(i))
+  .y0(0)
+  .y1(d => bScale(d.deaths));
+
+  d3.select("#bAreaChart")
+  .join("path")
+  .attr('d', bAreaGenerator(data))
+
+
   // TODO: Select and update the scatterplot points
+
+  //Defined scatterplot axes
+  let sx = d3.axisBottom(aScale).ticks(5)
+  let sy = d3.axisLeft(bScale).ticks(5)
+
+  d3.selectAll("#scatterplot > #y-axis")
+    .call(sy)
+  d3.selectAll("#scatterplot > #x-axis")
+    .call(sx) 
   
+
+
+    //Define data points
+  let dotPoint = d3.select("#scatterplot").selectAll('circle')
+    .data(data)
+    .attr('cx', d => aScale(d.cases)) //#Assigns data to points
+    .attr('cy', d => bScale(d.deaths)) //#Assigns data to points
+    .attr('r', '5') //Set radius of points
+    
+  dotPoint.exit().remove()
+
+  let newDots = dotPoint.enter().append('circle')
+    .attr('cx', d => aScale(d.cases))
+    .attr('cy', d=> bScale(d.deaths))
+    .attr('r', '5')
+
   // ****** TODO: PART IV ******
 }
 
